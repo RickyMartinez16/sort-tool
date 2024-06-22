61a71c9914e559eef5c417c6c7730e9f331d173f
Ricky Martinez <RMartinezW16@gmail.com>
2024-06-22 14:45:36 -0400

    sort updated

--- sort.js
+++ sort.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 
 // Function to read, sort, and output file content
-const sortFile = (inputFile) => {
+const sortFile = (inputFile, numLines = 5) => {
   fs.readFile(inputFile, 'utf8', (err, data) => {
     if (err) throw err;
 
     // Split file content into lines and sort them
-    const sortedLines = data.split('\n').sort((a, b) => a.localeCompare(b));
+    let sortedLines = data.split('\n').sort((a, b) => a.localeCompare(b));
 
-    // Output the sorted lines
-    sortedLines.forEach(line => console.log(line));
+    // Remove duplicate lines (uniq functionality)
+    sortedLines = sortedLines.filter((line, index, self) => self.indexOf(line) === index);
+
+    // Output the first numLines lines (head functionality)
+    sortedLines.slice(0, numLines).forEach(line => console.log(line));
   });
 };
 
-// Run the sortFile function with 'words.txt' as input
+// Run the sortFile function with 'words.txt' as input and 5 lines to display
 sortFile('words.txt');
